0afcca6af04014446bcb38f96c123572e2f788ee
WtoJglg4 <glazovv81@gmail.com>
2023-11-04 16:05:55 +0300

    Added some comments

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -3,14 +3,16 @@
 #include <map>
 using namespace std;
 
+//экземпляр элемента очереди с приоритетом
 struct PQueue{
-    int data;
-    PQueue *next;
+    int data; //значение элемента
+    PQueue *next; //указатель на следующий элемент
 };
 
 int count = 0;
-PQueue *front, *rear;
+PQueue *front, *rear; //начало, конец
 
+//добавление элемента(неприоритетное)
 void PQInsert(int a){
     PQueue *q = new PQueue;
     if (count == 0){
@@ -24,6 +26,7 @@ void PQInsert(int a){
     count++;
 }
 
+//печать всей очереди
 void PQPrint(){
     PQueue *curr = front;
     cout << "Queue: ";
@@ -34,11 +37,16 @@ void PQPrint(){
     cout << endl;
 }
 
+//удаление элемента(приоритетное)
 int PQDelete(int shortest[]) {
+    //пустая очередь
     if (!front) {
         cerr << "queue is empty\n";
         exit(1);
     } else {
+        //непустая
+
+        //поиск элемента, с минимальным значением shortest
         int minShortest = shortest[front->data];
         int minKey = front->data;
         PQueue *curr = front->next;
@@ -103,6 +111,7 @@ bool PQEmpty(){
     return !front;
 }
 
+//печать shortest или pred
 void PrintTable(int arr[], int size, map<int, char> nodeNames, string tableName){
     cout << tableName << ":\n";
     for(int i = 0; i < size; i++){
@@ -125,6 +134,7 @@ void PrintTable(int arr[], int size, map<int, char> nodeNames, string tableName)
 int main(){    
     //это граф из лекции
     //H = head
+    //ключи - численные номера вершин в shortest, pred. значения - соответствующие буквенные
     const map<int, char> nodeNames = { {0, 'S'}, {1, 'T'}, {2, 'Y'}, {3, 'X'}, {4, 'C'}, {-1, 'H'} };
     int Graph[5][10] = {    {0, 6, 4, 0, 0},
                             {0, 0, 2, 3, 0},
@@ -140,30 +150,40 @@ int main(){
     //                         {0, 0, 1, 0, 0},
     //                         {0, 0, 0, 5, 0}};
 
-    int shortest[5];
-    int pred[5];
-    shortest[0] = 0;
-    pred[0] = -1;
+    int shortest[5]; //массив кратчайших путей до каждой из вершин
+    int pred[5]; //предыдущая вершина на найденном кратчайшем пути для каждой из вершин
+    shortest[0] = 0; //для head
+    pred[0] = -1; //тоже для head
     for (int i = 1; i <= 4; i++){
-        shortest[i] = INT_MAX;
+        shortest[i] = INT_MAX; //для всех остальных
         pred[i] = -1;
     }  
 
-    PQueue *q;
-    front = q;
-    rear = q;
+    PQueue *q; //очередь с приоритетом вершин
+    front = q; //первый элемент
+    rear = q; //последний
+
+    //все узлы поместить в очередь
     for (int i = 0; i <= 4; i++){
         PQInsert(i);
     }
     // PQPrint();
+
+    //пока не пустая очередь
     while(!PQEmpty()){
+        //удалить и вернуть элемент с наибольшим приоритетом
+        //замечание: чем МЕНЬШЕ значение shortest[i], тем больше приоритет вершины
         int u = PQDelete(shortest);
-        // cout << u << endl;
-        // PQPrint();
         
+        //по смежным с вершинам взятой из очереди
         for(int i = 0; i < size(Graph[u]); i++){
+            //если есть ребро
             if (Graph[u][i] > 0){
+                //смежная вершина v и вес ребра (u, v)
                 int v = i, weight = Graph[u][i]; 
+
+                //Relax
+                //путь (u, v) короче, чем записанный ранее в sortest -> обновить shortest и pred
                 if (shortest[u] + weight < shortest[v]){
                     shortest[v] = shortest[u] + weight;
                     pred[v] = u;
@@ -171,6 +191,8 @@ int main(){
             }
         }
     }
+
+    //форматированный вывод shortest и pred
     PrintTable(shortest, size(shortest), nodeNames, "Shortest");
     PrintTable(pred, size(pred), nodeNames, "Pred");
 }
